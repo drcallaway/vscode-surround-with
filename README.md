e1def59a3a9a168a7cf2eb8c4df3ea97941cf024
dcallaway <dustin_callaway@intuit.com>
2019-09-25 19:42:11 -0600

    Add initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vscode-surround-with
+# Surround With
+Wraps a code selection in an `if` or `try/catch` block. Adds some bug fixes to an [unsupported project](https://github.com/JuoCode/vscode-surround).
+
+## Install
+Install from VS Code with `cmd+shift+x` (or `ctrl+shift+x` on Windows), search for `vscode-surround-with`, and click "Install".
+
+## Usage
+Select text to wrap with an `if` or `try/catch` block, press `cmd+shift+p` (`ctrl+shift+p` on Windows), and select one of the following:
+* Surround with if
+* Surround with try/catch
+
+## Release Notes
+### 1.0.0
+First release
---
+++ src/extension.ts
@@ -0,0 +1,117 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    let ifSurround = vscode.commands.registerCommand('extension.surroundWithIf', async () => {
+        var { editor, startLine, endLine, selection } = await processSelection();
+
+        // surround with if
+        await editor.edit(currentText => {
+            currentText.replace(startLine.range.union(endLine.range), surroundWithIf(selection));
+        });
+
+        // highlight if condition
+        await editor.edit(() => {
+            editor.selection = new vscode.Selection(
+                new vscode.Position(startLine.lineNumber, startLine.firstNonWhitespaceCharacterIndex + 4),
+                new vscode.Position(startLine.lineNumber, startLine.firstNonWhitespaceCharacterIndex + 13)
+            );
+        });
+    });
+
+    let trySurround = vscode.commands.registerCommand('extension.surroundWithTry', async () => {
+        var { editor, startLine, endLine, selection, lines } = await processSelection();
+
+        // surround with try/catch
+        await editor.edit(currentText => {
+            currentText.replace(startLine.range.union(endLine.range), surroundWithTry(selection));
+        });
+
+        // highlight catch variable
+        await editor.edit(() => {
+            const catchBodyLineNumber = startLine.lineNumber + lines.length + 1;
+            const catchLine = editor.document.lineAt(catchBodyLineNumber);
+            editor.selection = new vscode.Selection(
+                new vscode.Position(catchLine.lineNumber, catchLine.firstNonWhitespaceCharacterIndex + 9),
+                new vscode.Position(catchLine.lineNumber, catchLine.firstNonWhitespaceCharacterIndex + 14)
+            );
+        });
+    });
+
+    context.subscriptions.push(ifSurround);
+    context.subscriptions.push(trySurround);
+}
+
+async function processSelection() {
+    const editor = vscode.window.activeTextEditor;
+    let selection = editor.selection;
+    if (selection.isEmpty) {
+        selection = getCurrentLineSelection(selection);
+    }
+    let lines = getSelectLines(selection);
+    const startLine = lines[0];
+    let endLine = lines[lines.length - 1];
+    if (selection.end.character === 0) {
+        // if end selection is at the start of the last line, update selection to not include that line
+        await editor.edit(currentText => {
+            editor.selection = new vscode.Selection(new vscode.Position(startLine.lineNumber, 0), new vscode.Position(endLine.lineNumber - 1, endLine.range.end.character));
+        });
+        selection = editor.selection;
+        lines = getSelectLines(selection);
+        endLine = lines[lines.length - 1];
+    }
+    return { editor, startLine, endLine, selection, lines };
+}
+
+function surroundWithIf(selection: vscode.Selection) {
+    let lines = getSelectLines(selection);
+    const { prefix } = getPrefixAndIndent(lines[0]);
+    return [
+        `${prefix}if (condition) {`,
+        ...indentLines(lines),
+        `${prefix}}`
+    ].join('\n');
+}
+
+function surroundWithTry(selection: vscode.Selection) {
+    let lines = getSelectLines(selection);
+    const { prefix, indent } = getPrefixAndIndent(lines[0]);
+    return [
+        `${prefix}try {`,
+        ...indentLines(lines),
+        `${prefix}} catch (error) {`,
+        `${prefix}${indent}// handle error`,
+        `${prefix}}`,
+    ].join('\n');
+}
+
+function getCurrentLineSelection(selection: vscode.Selection) {
+    const pos = selection.active;
+    let line = vscode.window.activeTextEditor.document.lineAt(pos.line);
+    return line.range as vscode.Selection;
+}
+
+function getPrefixAndIndent(line: vscode.TextLine) {
+    let indentLength = line.firstNonWhitespaceCharacterIndex;
+    const { tabSize, insertSpaces } = vscode.window.activeTextEditor.options;
+    return {
+        indent: new Array((tabSize as number) + 1).join(insertSpaces ? ' ' : '\t'),
+        prefix: new Array(indentLength + 1).join(insertSpaces ? ' ' : '\t')
+    };
+}
+
+function getSelectLines(selection: vscode.Selection): vscode.TextLine[] {
+    let lines = [];
+    for (let i = selection.start.line; i < selection.end.line + 1; i++) {
+        lines.push(vscode.window.activeTextEditor.document.lineAt(i));
+    }
+    return lines;
+}
+
+function indentLines(lines: vscode.TextLine[]) {
+    const { tabSize, insertSpaces } = vscode.window.activeTextEditor.options;
+    let prefix = insertSpaces ? new Array((tabSize as number) + 1).join(' ') : '\t';
+    return lines.map(line => prefix + line.text);
+}
